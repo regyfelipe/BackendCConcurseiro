e30de148a0e3d18b42e63c908574bc53fe2b9ac8
Regy Felipe <139698924+regyfelipe@users.noreply.github.com>
2024-11-03 23:41:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/userRoutes.js'; // Importando as rotas de usuário
+import userRoutes from './routes/userRoutes.js'; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Habilita o CORS para todas as origens
 app.use(cors());
 
-// Habilita o uso de JSON
 app.use(express.json());
 
-// Usando as rotas de usuário
-app.use('/api', userRoutes); // Prefixo para todas as rotas de usuário
+app.use('/api', userRoutes); 
 
-// Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
